dcabdd5dfa495f795310c9ae06afab5c65c7a3bc
Latrell <lll4xy@virginia.edu>
2021-11-23 11:22:30 -0500

    cleaned up component structure

---
+++ src/components/Anecdotes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdotes = () => {
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                <div>
+                    {anecdote.content}
+                </div>
+                <div>
+                    has {anecdote.votes}
+                    <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+                </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Anecdotes
