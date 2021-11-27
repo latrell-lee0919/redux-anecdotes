9f7b7a7bed3534d96f61e12c7f2995ded6a29338
Latrell <lll4xy@virginia.edu>
2021-11-27 13:46:06 -0500

    exercises 6.13-6.14

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NewAnecdote from './components/NewAnecdote'
 import Anecdotes from './components/Anecdotes'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { initializeData } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+    .getAll().then(anecdotes => dispatch(initializeData(anecdotes)))
+  }, [dispatch])
 
   return (
     <div>
